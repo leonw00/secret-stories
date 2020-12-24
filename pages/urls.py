531f60fda3fdb68cc23dc77d1edc79c24a21a60f
leo <leonardow1908@gmail.com>
2020-12-24 16:21:26 -0600

    added writing page

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import LandingPageView
+from .views import LandingPageView, ReadingPageView, WritingPageView
 
 urlpatterns = [
     path('', LandingPageView.as_view(), name = 'landing'),
+    path('reading/', ReadingPageView.as_view(), name = 'reading'),
+    path('writing/', WritingPageView.as_view(), name = 'writing'),
 ]   
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,9 @@ from django.views.generic import TemplateView
 
 class LandingPageView(TemplateView):
     template_name = 'landing.html'
+
+class ReadingPageView(TemplateView):
+    template_name = 'reading.html'
+
+class WritingPageView(TemplateView):
+    template_name = 'writing.html'
